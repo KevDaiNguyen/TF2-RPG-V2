9fe2c20b48da4c5710cf339082b77ff75ff924cf
Kevin Nguyen <8killernguyen8@gmail.com>
2022-06-18 16:59:10 -0500

    damage calculations and ui additions

--- Assets/Scripts/ClassData.cs
+++ Assets/Scripts/ClassData.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class ClassData : MonoBehaviour
 {
+    [Header("---Class Identifier---")]
     public string className;
-
+    [Header("---Variable Health---")]
     private int defaultHealth;
     public int currentHealth;
-
-    public int slotSpace;
-
+    [Header("---Variable Speed---")]
     private int defaultSpeed;
     public int currentSpeed;
-
+    [Header("---Total slots should be <= 4---")]
+    public int slotSpace;
+    [Header("---Damage Calculations---")]
+    public float bulletResist;
+    public float explosiveResist;
+    public float fireResist;
+    public float meleeResist;
+    public float bulletVulnerability;
+    public float explosiveVulnerability;
+    public float fireVulnerability;
+    public float meleeVulnerability;
+    [Header("---Basic Class Sprite---")]
     public Sprite classSprite;
-
+    [Header("---Active when changing classes---")]
     public bool switchedClass = false;
-
+    [Header("---Only active on death---")]
     public bool classDead;
-
+    [Header("---Holds all weapons in slots---")]
     public WeaponMaker primarySlot;
     public WeaponMaker secondarySlot;
     public WeaponMaker meleeSlot;
@@ -30,6 +40,10 @@ public class ClassData : MonoBehaviour
     {
         className = "Spy";
         slotSpace = 1;
+        bulletResist = 0;
+        explosiveResist = 0;
+        fireResist = 0;
+        meleeResist = 0;
         primarySlot = WeaponDatabase.spyPrimaries[0];
         secondarySlot = WeaponDatabase.spySecondaries[0];
         meleeSlot = WeaponDatabase.spyMelees[0];
@@ -144,14 +158,14 @@ public class ClassData : MonoBehaviour
         }
     }
 
-    public void ChangeHealth(int healthChangeNum, bool fromMedigun, bool isQuickFix)
+    public void TakeHealing(int healthChangeNum, bool fromMedigun, bool isQuickFix)
     {
-        if ((currentHealth + healthChangeNum) <= 0)
+        if (healthChangeNum < 0)
         {
-            currentHealth = 0;
-            classDead = true;
+            healthChangeNum *= -1;
         }
-        else if ((currentHealth + healthChangeNum) > defaultHealth && !fromMedigun)
+
+        if ((currentHealth + healthChangeNum) > defaultHealth && !fromMedigun)
         { 
             currentHealth = defaultHealth;
         }
@@ -171,6 +185,217 @@ public class ClassData : MonoBehaviour
             currentHealth += healthChangeNum;
         }
     }
+
+    public void TakeDamage(float damageNum, string damageType, bool miniCrit, bool Crit)
+    {
+        float critDamage = 0;
+        float resistDamage = 0;
+
+        if (miniCrit && Crit)
+        {
+            critDamage = damageNum * 3;
+        }
+        else if (miniCrit && !Crit)
+        {
+            critDamage = damageNum * 1.35f;
+        }
+        else if (!miniCrit && Crit) 
+        {
+            critDamage = damageNum * 3;
+        }
+        else if (!miniCrit && !Crit) 
+        {
+            critDamage = 0;
+        }
+
+        switch (damageType)
+        {
+            case "Bullet":
+                resistDamage = damageNum * (bulletResist * bulletVulnerability);
+                break;
+            case "Explosive":
+                resistDamage = damageNum * (explosiveResist * explosiveVulnerability);
+                break;
+            case "Fire":
+                resistDamage = damageNum * (fireResist * fireVulnerability);
+                break;
+            case "Melee":
+                resistDamage = damageNum * (meleeResist * meleeVulnerability);
+                break;
+        }
+
+        Mathf.RoundToInt(critDamage);
+        Mathf.RoundToInt(resistDamage);
+
+        if (currentHealth - ((int)critDamage + (int)(resistDamage)) <= 0)
+        {
+            currentHealth = 0;
+            classDead = true;
+        }
+        else
+        {
+            currentHealth -= ((int)critDamage + (int)(resistDamage));
+        }
+    }
+
+    public void ChangeResist(float resistIncrease, string resistType, bool shouldIncrease)
+    {
+        switch (resistType)
+        {
+            case "Bullet":
+                if (bulletResist != 0 && shouldIncrease)
+                {
+                    bulletResist *= (1 - resistIncrease);
+                }
+                else if (bulletResist == 0 && shouldIncrease)
+                {
+                    bulletResist += (1 - resistIncrease);
+                }
+                else if (bulletResist != 0 && !shouldIncrease)
+                {
+                    bulletResist /= (1 - resistIncrease);
+                }
+                else if (bulletResist == 0 && !shouldIncrease)
+                {
+                    bulletResist = 0;
+                }
+                break;
+            case "Explosive":
+                if (explosiveResist != 0 && shouldIncrease)
+                {
+                    explosiveResist *= (1 - resistIncrease);
+                }
+                else if (explosiveResist == 0 && shouldIncrease) 
+                {
+                    explosiveResist += (1 - resistIncrease);
+                }
+                else if (explosiveResist != 0 && !shouldIncrease)
+                {
+                    explosiveResist /= (1 - resistIncrease);
+                }
+                else if (explosiveResist == 0 && !shouldIncrease)
+                {
+                    explosiveResist = 0;
+                }
+                break;
+            case "Fire":
+                if (fireResist != 0 && shouldIncrease)
+                {
+                    fireResist *= (1 - resistIncrease);
+                }
+                else if (fireResist == 0 && shouldIncrease)
+                {
+                    fireResist += (1 - resistIncrease);
+                }
+                else if (fireResist != 0 && !shouldIncrease)
+                {
+                    fireResist /= (1 - resistIncrease);
+                }
+                else if (fireResist == 0 && !shouldIncrease)
+                {
+                    fireResist = 0;
+                }
+                break;
+            case "Melee":
+                if (meleeResist != 0 && shouldIncrease)
+                {
+                    meleeResist *= (1 - resistIncrease);
+                }
+                else if (meleeResist == 0 && shouldIncrease)
+                {
+                    meleeResist += (1 - resistIncrease);
+                }
+                else if (meleeResist != 0 && !shouldIncrease)
+                {
+                    meleeResist /= (1 - resistIncrease);
+                }
+                else if (meleeResist == 0 && !shouldIncrease)
+                {
+                    meleeResist = 0;
+                }
+                break;
+        }
+    }
+
+    public void ChangeVulnerability(float vulerabilityIncrease, string resistType, bool shouldIncrease)
+    {
+        switch (resistType)
+        {
+            case "Bullet":
+                if (bulletVulnerability != 0 && shouldIncrease)
+                {
+                    bulletVulnerability *= (1 + vulerabilityIncrease);
+                }
+                else if (bulletVulnerability == 0 && shouldIncrease)
+                {
+                    bulletVulnerability += (1 + vulerabilityIncrease);
+                }
+                else if (bulletVulnerability != 0 && !shouldIncrease)
+                {
+                    bulletVulnerability /= (1 + vulerabilityIncrease);
+                }
+                else if (bulletVulnerability == 0 && !shouldIncrease)
+                {
+                    bulletVulnerability = 0;
+                }
+                break;
+            case "Explosive":
+                if (explosiveVulnerability != 0 && shouldIncrease)
+                {
+                    explosiveVulnerability *= (1 + vulerabilityIncrease);
+                }
+                else if (explosiveVulnerability == 0 && shouldIncrease)
+                {
+                    explosiveVulnerability += (1 + vulerabilityIncrease);
+                }
+                else if (explosiveVulnerability != 0 && !shouldIncrease)
+                {
+                    explosiveVulnerability /= (1 + vulerabilityIncrease);
+                }
+                else if (explosiveVulnerability == 0 && !shouldIncrease)
+                {
+                    explosiveVulnerability = 0;
+                }
+                break;
+            case "Fire":
+                if (fireVulnerability != 0 && shouldIncrease)
+                {
+                    fireVulnerability *= (1 + vulerabilityIncrease);
+                }
+                else if (fireVulnerability == 0 && shouldIncrease)
+                {
+                    fireVulnerability += (1 + vulerabilityIncrease);
+                }
+                else if (fireVulnerability != 0 && !shouldIncrease)
+                {
+                    fireVulnerability /= (1 + vulerabilityIncrease);
+                }
+                else if (fireVulnerability == 0 && !shouldIncrease)
+                {
+                    fireVulnerability = 0;
+                }
+                break;
+            case "Melee":
+                if (meleeVulnerability != 0 && shouldIncrease)
+                {
+                    meleeVulnerability *= (1 + vulerabilityIncrease);
+                }
+                else if (meleeVulnerability == 0 && shouldIncrease)
+                {
+                    meleeVulnerability += (1 + vulerabilityIncrease);
+                }
+                else if (meleeVulnerability != 0 && !shouldIncrease)
+                {
+                    meleeVulnerability /= (1 + vulerabilityIncrease);
+                }
+                else if (meleeVulnerability == 0 && !shouldIncrease)
+                {
+                    meleeVulnerability = 0;
+                }
+                break;
+        }
+    }
+
 
     public void RespawnClass()
     {
